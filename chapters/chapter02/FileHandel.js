b5af72b610fd2d5eb2540cdce2a5309ed03f8917
sai1004 <saikiran.bavandla@gmail.com>
2019-11-13 12:58:40 +0530

    commit

--- chapters/chapter02/FileHandel.js
+++ chapters/chapter02/FileHandel.js
@@ -1,21 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const express = require("express");
+const app = express();
 
-http
-  .createServer((req, res) => {
-    fs.readFile("demo.json", (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      res.writeHead(200, { "Content-Type": "text/json" });
-      res.write(data);
-      res.end();
-    });
-  })
-  .listen(3000, (err) => {
-      if (err) {
-          console.log('error while runing server')
-      } else{
-          console.log('server is listing on 3000')
-      }
+app.get("/", (req, res) => {
 
+  fs.readFile("demo.json", "UTF-8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    res.send(data);
+    res.end();
   });
+  
+
+});
+
+
+app.listen(3000, err => {
+  if (err) {
+    console.log("error while runing server");
+  } else {
+    console.log("server is listing on 3000");
+  }
+});
