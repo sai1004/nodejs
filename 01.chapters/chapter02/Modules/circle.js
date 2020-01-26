3401f8e9ad32dedcbeaed7f0f2412d90749191ee
sai1004 <saikiran.bavandla@gmail.com>
2020-01-26 16:03:47 +0530

    commit

---
+++ 01.chapters/chapter02/Modules/circle.js
@@ -0,0 +1,7 @@
+const { PI } = Math;
+
+exports.area = r => PI * r ** 2;
+
+exports.circumference = r => 2 * PI * r;
+
+exports.imfromCriles = "im from circle module";
---
+++ 01.chapters/chapter02/Modules/foo.js
@@ -0,0 +1,6 @@
+const circle = require("./circle.js");
+const Square = require("./circle.js");
+
+console.log(
+  `The Area Of a Cricle Of Radius 4 Is ${circle.area(4)} ${circle.imfromCriles}`
+);
