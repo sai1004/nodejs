1d4984081ede6152906a90bf6f05619e657b078e
sai1004 <saikiran.bavandla@gmail.com>
2020-03-14 20:21:57 +0530

    commit

--- 01.chapters/chapter05/ExpressJs/chapter01/app.js
+++ 01.chapters/chapter05/ExpressJs/chapter01/app.js
@@ -17,26 +17,21 @@ app.use(logger("common"));
 app.use(bodyParser.json());
 
 app.get("/contacts", (req, res) => {
-
+  console.log(req.url);
   let getParams = url.parse(req.url, true).query;
-  
+
+  res.setHeader("content-type", "application/json");
+
   if (Object.keys(getParams).length === 0) {
-
-    res.setHeader("content-type", "application/json");
     res.end(JSON.stringify(contacts.list()));
-
   } else {
-
-    res.setHeader("content-type", "application/json");
     res.end(
       JSON.stringify(contacts.queryByArg(getParams.arg, getParams.value))
     );
-    
   }
-
+  
 });
 
-
 http.createServer(app).listen(3000, () => {
-  console.log("Express server listening on port 3000");
+  console.log("Express server listening on port http://127.0.0.1:3000");
 });
