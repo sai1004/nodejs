6d0b4a9bbd452268cfa20a453bdf527f545b98dd
saikiran <35389534+sai1004@users.noreply.github.com>
2020-03-14 09:50:23 +0530

    Create 01.math.js

---
+++ 01.chapters/chapter02/Modules/01.math.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+function multiply(c, d) {
+  return c * d;
+}
+
+function sqaure(e) {
+  return e ** 2;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  sqaure
+};
